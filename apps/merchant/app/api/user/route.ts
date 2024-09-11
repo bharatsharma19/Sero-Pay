9647beba4f906e41c5ad283db24a847a364fb0cb
Bharat Sharma <82795831+Bharatsharma19@users.noreply.github.com>
2024-09-11 14:05:13 +0530

    Refactor user route to return a specific message for the all merchants endpoint

--- apps/merchant/app/api/user/route.ts
+++ apps/merchant/app/api/user/route.ts
@@ -1,10 +1,5 @@
 import { NextResponse } from "next/server";
-import db from "@repo/db/client";
 
 export const GET = async () => {
-  const allMerchants = await db.merchant.findMany();
-
-  return NextResponse.json({
-    data: allMerchants,
-  });
+  return NextResponse.json({ message: "All Merchants Endpoint" });
 };
