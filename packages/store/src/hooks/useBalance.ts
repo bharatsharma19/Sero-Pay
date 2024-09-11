fa5e92a9102753515c63e689f2daa7d25cd3671f
Bharat Sharma <82795831+Bharatsharma19@users.noreply.github.com>
2024-09-11 14:06:56 +0530

    fix: New Branch with Issues Fixed

--- packages/store/src/hooks/useBalance.ts
+++ packages/store/src/hooks/useBalance.ts
@@ -2,5 +2,7 @@ import { useRecoilValue } from "recoil";
 import { balanceAtom } from "../atoms/balance";
 
 export const useBalance = () => {
-  return useRecoilValue(balanceAtom);
+  const finalValue = useRecoilValue(balanceAtom);
+
+  return finalValue;
 };
